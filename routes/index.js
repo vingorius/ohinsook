6035854702dc503f1670c87a1dd4789c2ff619aa
vingorius <vingorius@gmail.com>
2015-11-23 00:21:03 +0900

    2nd update

--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('work');
+    res.render('work');
 });
 
 router.get('/:page', function(req, res, next) {
-  var page = req.params.page;
-  // console.log(page);
-  res.render(page);
+    var page = req.params.page;
+    // console.log(page);
+    res.render(page);
 });
 
 router.get('/:folder/:page', function(req, res, next) {
-  var folder = req.params.folder;
-  var page = req.params.page;
-  console.log(folder,page);
-  res.render(folder + '/' + page);
+    var folder = req.params.folder;
+    var page = req.params.page;
+    console.log(folder, page);
+    res.render(folder + '/' + page);
 });
 
 module.exports = router;
