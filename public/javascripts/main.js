093b705b3c6a3f1df22b98041370f9456c9bf317
vingorius <vingorius@gmail.com>
2015-11-23 14:05:18 +0900

    google analytics

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,7 +5,12 @@ $(document).ready(function() {
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-    ga('create', 'UA-69023226-1', 'auto');
+    // ga('create', 'UA-69023226-1', 'auto');
+    // ga('send', 'pageview');
+    ga('create', 'UA-69023226-1', 'auto', {'allowLinker': true});
+    ga('create', 'UA-69023226-2', 'auto', {'allowLinker': true});
+    ga('require', 'linker');
+    ga('linker:autoLink', ['oh.citygoat.net', 'www.citygoat.net'] );
     ga('send', 'pageview');
 
     //set clicked navbar as active x
