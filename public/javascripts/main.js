861b481c16562427882ebeeae90e7f00a5a4a43e
vingorius <vingorius@gmail.com>
2015-11-28 14:44:38 +0900

    add facebook

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -17,4 +17,21 @@ $(document).ready(function() {
         $('ul.nav.navbar-nav').find('a[href="' + location.pathname + '"]')
             .closest('li').addClass('active');
     });
+
+    //facebook
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId      : '1582302325351263',
+        xfbml      : true,
+        version    : 'v2.5'
+      });
+    };
+
+    (function(d, s, id){
+       var js, fjs = d.getElementsByTagName(s)[0];
+       if (d.getElementById(id)) {return;}
+       js = d.createElement(s); js.id = id;
+       js.src = "//connect.facebook.net/en_US/sdk.js";
+       fjs.parentNode.insertBefore(js, fjs);
+     }(document, 'script', 'facebook-jssdk'));
 });
