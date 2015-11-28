b71f664ec8743e3d988ca342fe2616ff4c450250
vingorius <vingorius@gmail.com>
2015-11-28 19:16:19 +0900

    add facebook

--- start.sh
+++ start.sh
@@ -6,5 +6,5 @@ export NODE_ENV=production
 # Forever As Superuser
 PORT=3000 forever start  --minUptime 1000 --spinSleepTime 1000 \
                --uid "vingorius" -l "/tmp/ohinsook.log" --append \
-               -c "/usr/local/bin/nodemon" bin/www
+               -c "/usr/local/bin/nodemon -c routes" bin/www
 
